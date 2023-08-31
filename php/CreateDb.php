a2f963900aad6c69411d183c174f0b82ca4e47cf
Megi <138001148+megigremi@users.noreply.github.com>
2023-08-31 14:24:33 +0200

    Add files via upload

---
+++ php/CreateDb.php
@@ -0,0 +1,98 @@
+<?php
+
+
+class CreateDb
+{
+        public $servername;
+        public $username;
+        public $password;
+        public $dbname;
+        public $tablename;
+        public $con;
+
+
+        // class constructor
+    public function __construct(
+        $dbname = "Newdb",
+        $tablename = "Productdb",
+        $servername = "localhost",
+        $username = "root",
+        $password = ""
+    )
+    {
+      $this->dbname = $dbname;
+      $this->tablename = $tablename;
+      $this->servername = $servername;
+      $this->username = $username;
+      $this->password = $password;
+
+      // create connection
+        $this->con = mysqli_connect($servername, $username, $password);
+
+        // Check connection
+        if (!$this->con){
+            die("Connection failed : " . mysqli_connect_error());
+        }
+
+        // query
+        $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+
+        // execute query
+        if(mysqli_query($this->con, $sql)){
+
+            $this->con = mysqli_connect($servername, $username, $password, $dbname);
+
+            // sql to create new table
+            $sql = " CREATE TABLE IF NOT EXISTS $tablename
+                            (id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                             product_name VARCHAR (25) NOT NULL,
+                             product_price FLOAT,
+                             product_image VARCHAR (100),
+                             product_quantity INT(11),
+                             product_des VARCHAR (25) NOT NULL
+
+                            );";
+
+            if (!mysqli_query($this->con, $sql)){
+                echo "Error creating table : " . mysqli_error($this->con);
+            }
+
+        }else{
+            return false;
+        }
+    }
+
+    // get product from the database
+    public function getData(){
+        $sql = "SELECT * FROM $this->tablename";
+
+        $result = mysqli_query($this->con, $sql);
+
+        if(mysqli_num_rows($result) > 0){
+            return $result;
+        }
+    }
+
+
+    public function getDataWithLimit($offset, $limit)
+    {
+        $sql = "SELECT * FROM $this->tablename LIMIT $limit OFFSET $offset";
+        $result = mysqli_query($this->con, $sql);
+        return $result;
+    }
+
+    // Method to get the total number of products
+    public function getTotalProducts()
+    {
+        $sql = "SELECT COUNT(*) as total FROM $this->tablename";
+        $result = mysqli_query($this->con, $sql);
+        $data = mysqli_fetch_assoc($result);
+        return $data['total'];
+    }
+
+}
+
+
+
+
+
